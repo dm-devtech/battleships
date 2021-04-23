2d0960713758ffd0faa2ad683a9442883a0b813b
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-23 18:42:26 +0100

    chore: removing console logs and comments, reset grid in Battleships class

--- src/Battleships.js
+++ src/Battleships.js
@@ -3,17 +3,13 @@ class Battleships {
   constructor(){
   }
 
-  test() {
-    console.log("test")
-  }
-
   grid() {
     const grid = [[0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
-    ["B",0,0,0,0,0,0,0,0,0],
-    ["D",0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -22,19 +18,17 @@ class Battleships {
   };
 
   convertCoordinate(coordinate) {
-    const x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"] // 0
-    const y = [1,2,3,4,5,6,7,7,8,9,10] // 4
-    console.log(x,y)
+    const x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    const y = [1,2,3,4,5,6,7,7,8,9,10]
     const coordinateSplit = coordinate.split("")
-    const column = x.indexOf(coordinateSplit[0])  // 0
-    const row = y.indexOf(parseInt(coordinateSplit[1])) // 4
+    const column = x.indexOf(coordinateSplit[0])
+    const row = y.indexOf(parseInt(coordinateSplit[1]))
     return [row, column]
   }
 
   turn(coordinate) {
     const [row, column] = this.convertCoordinate(coordinate)
     const grid = this.grid()
-    console.log(grid[row][column])
     if(grid[row][column] === "D" || grid[row][column] === "B"){
       return "Hit"
     }else {
