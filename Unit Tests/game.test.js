5ef522441e42f1485b006363f82700924f8b1c18
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-24 17:05:46 +0100

    feat: random allocation of ships for computer player set up

--- Unit Tests/game.test.js
+++ Unit Tests/game.test.js
@@ -27,19 +27,33 @@ describe('Scorecard class', function() {
 
   describe('testing game', function() {
     it('testing hit ship', function() {
-      grid = jest.spyOn(game, 'grid').mockReturnValue(gridMock())
-      turn = jest.spyOn(game, 'turn')
+      const shipPlacement = jest.spyOn(game, 'allocateShipsRandomly').mockImplementation(() => gridMock())
+      const turn = jest.spyOn(game, 'turn')
+      game.gameGrid = shipPlacement()
       expect(game.turn("A5")).toEqual("Hit")
       expect(turn).toHaveBeenCalled()
       expect(turn).toHaveBeenCalledTimes(1);
     });
 
     it('testing missing a ship', function() {
-      grid = jest.spyOn(game, 'grid').mockReturnValue(gridMock())
-      turn = jest.spyOn(game, 'turn')
+      const shipPlacement = jest.spyOn(game, 'allocateShipsRandomly').mockImplementation(() => gridMock())
+      game.gameGrid = shipPlacement()
+      const turn = jest.spyOn(game, 'turn')
       expect(game.turn("A6")).toEqual("Miss")
       expect(turn).toHaveBeenCalled()
       expect(turn).toHaveBeenCalledTimes(1);
+    });
+
+    it('testing random grid created', function() {
+      const shipPlacement = jest.spyOn(game, 'allocateShipsRandomly').mockImplementation(() => gridMock())
+      game.gameGrid = shipPlacement()
+      console.log(game.gameGrid)
+      const turn = jest.spyOn(game, 'turn')
+      expect(game.turn("E9")).toEqual("Hit")
+      expect(turn).toHaveBeenCalled()
+      expect(turn).toHaveBeenCalledTimes(1);
+      expect(shipPlacement).toHaveBeenCalled()
+      expect(shipPlacement).toHaveBeenCalledTimes(1);
     });
   })
 
