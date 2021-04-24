7f4fe1a97494340ffe9edcaa54361bd24a786a6f
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-24 17:05:32 +0100

    feat: random allocation of ships for computer player set up

--- src/Battleships.js
+++ src/Battleships.js
@@ -1,9 +1,10 @@
 class Battleships {
 
   constructor(){
+    this.gameGrid = this.allocateShipsRandomly()
   }
 
-  grid() {
+  blankGrid() {
     const grid = [[0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -17,6 +18,31 @@ class Battleships {
     return grid
   };
 
+  randomNumber(max){
+    return Math.floor(Math.random() * max)
+  }
+
+  randomCoordinates() {
+    let [ship1Row, ship1Column] = [this.randomNumber(8), this.randomNumber(5)]
+    let [ship2Row, ship2Column] = [this.randomNumber(8), this.randomNumber(5)]
+    let [ship3Row, ship3Column] = [this.randomNumber(8), this.randomNumber(5)]
+    return [ship1Row, ship1Column, ship2Row, ship2Column, ship3Row, ship3Column]
+  }
+
+  allocateShipsRandomly() {
+    let gameGrid = this.blankGrid()
+    let [ship1Row, ship1Column, ship2Row, ship2Column, ship3Row, ship3Column] = this.randomCoordinates()
+
+    if(ship1Row === ship2Row) ship1Row += 1
+    if(ship2Row === ship3Row) ship2Row += 1
+    if(ship3Row === ship1Row) ship3Row += 1
+
+    gameGrid[ship1Row].splice([ship1Column], 5, 'B', 'B', 'B', 'B', 'B')
+    gameGrid[ship2Row].splice([ship2Column], 4, 'D', 'D', 'D', 'D')
+    gameGrid[ship3Row].splice([ship3Column], 4, 'D', 'D', 'D', 'D')
+    return gameGrid
+  }
+
   convertCoordinate(coordinate) {
     const x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
     const y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -28,8 +54,10 @@ class Battleships {
 
   turn(coordinate) {
     const [row, column] = this.convertCoordinate(coordinate)
-    const grid = this.grid()
-    return grid[row][column] === "D" || grid[row][column] === "B" ? "Hit" : "Miss"
+    const finalGrid = this.gameGrid
+    // console.log(this.assignedShips)
+    // console.log(finalGrid)
+    return finalGrid[row][column] === "D" || finalGrid[row][column] === "B" ? "Hit" : "Miss"
   }
 
 }
