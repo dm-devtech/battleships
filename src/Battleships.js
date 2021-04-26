96cfe367aef5f46f01403b902ef176bd0fb0333c
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-26 16:45:52 +0100

    feat: message sent when game is over

--- src/Battleships.js
+++ src/Battleships.js
@@ -32,14 +32,11 @@ class Battleships {
   allocateShipsRandomly() {
     let gameGrid = this.blankGrid()
     let [ship1Row, ship1Column, ship2Row, ship2Column, ship3Row, ship3Column] = this.randomCoordinates()
+    // rerun this if there arent 8 destoryer and 5 bship segments
+    if(!gameGrid[ship1Row].includes("B") && !gameGrid[ship1Row].includes("D")) gameGrid[ship1Row].splice([ship1Column], 5, 'B', 'B', 'B', 'B', 'B')
+    if(!gameGrid[ship2Row].includes("B") && !gameGrid[ship2Row].includes("D")) gameGrid[ship2Row].splice([ship2Column], 4, 'D', 'D', 'D', 'D')
+    if(!gameGrid[ship3Row].includes("B") && !gameGrid[ship3Row].includes("D")) gameGrid[ship3Row].splice([ship3Column], 4, 'D', 'D', 'D', 'D')
 
-    if(ship1Row === ship2Row) ship1Row += 1
-    if(ship2Row === ship3Row) ship2Row += 1
-    if(ship3Row === ship1Row) ship3Row += 1
-
-    gameGrid[ship1Row].splice([ship1Column], 5, 'B', 'B', 'B', 'B', 'B')
-    gameGrid[ship2Row].splice([ship2Column], 4, 'D', 'D', 'D', 'D')
-    gameGrid[ship3Row].splice([ship3Column], 4, 'D', 'D', 'D', 'D')
     return gameGrid
   }
 
@@ -52,20 +49,42 @@ class Battleships {
     return [row, column]
   }
 
+  isShipSunk(row) {
+    const sunk = this.gameGrid[row].includes("X") && !this.gameGrid[row].includes("B") && !this.gameGrid[row].includes("D")
+    return sunk
+  }
+
   hitMarker(turn, row, column) {
     if(turn === "Hit") this.gameGrid[row].splice([column], 1, 'X')
-    if(this.gameGrid[row].includes("X") && this.gameGrid[row].includes(0) && !this.gameGrid[row].includes("B") && !this.gameGrid[row].includes("D")) return "Sunk"
   }
 
   hitOrMiss(grid, row, column) {
       return grid[row][column] === "D" || grid[row][column] === "B" ? "Hit" : "Miss"
   }
 
+  isGameOver() {
+    let flattenedGrid = this.gameGrid.flat()
+    let hits = 0
+
+    for(let i = 0; i < flattenedGrid.length; i++){
+      if(flattenedGrid[i] === "X") hits += 1
+    }
+
+    const gameOver = hits === 13
+    return gameOver
+  }
+
   turn(coordinate) {
     const [row, column] = this.convertCoordinate(coordinate)
     const finalGrid = this.gameGrid
     const turnResult = this.hitOrMiss(finalGrid, row, column)
-    return this.hitMarker(turnResult, row, column) === "Sunk" ? "Sunk" : turnResult
+    this.hitMarker(turnResult, row, column)
+
+    if(this.isGameOver()){
+      return "Game Over"
+    }else {
+      return this.isShipSunk(row) && turnResult === "Hit" ? "Sunk" : turnResult
+    }
   }
 
 }
